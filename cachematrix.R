234af5b99961aef261ae4b3bcb5e03d7d3ff5b35
agchan <c_agnes_kh@yahoo.com.hk>
2018-12-26 19:39:35 +0800

    message

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## return the inverse of matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
