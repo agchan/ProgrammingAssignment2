c0906341644a360307ebf6ab2ff9770a29fb25ce
agchan <c_agnes_kh@yahoo.com.hk>
2018-12-27 11:58:08 +0800

    message

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
     z <-NULL
     setMat <- function(y) {
         x <<- y
-        z <- NULL #z is the invserse matrix
+        z <<- NULL #z is the invserse matrix
     }
     getMat <- function() x
     setInvMat <- function(inv) {
@@ -28,13 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## return the inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of matrix in x if the inverse exists
         inv <- x$getInvMat()
         if(!is.null(inv)){
-            message("getting cached inverse matrix")
+            message("getting cached data")
             return(inv)
         }
-        inv <- solve(x$getMat(),...)
+        ##Otherwise apply solve funct to obtain the inverse of matrix in x
+        mat <- x$getMat()
+        inv <- solve(mat,...)
         x$setInvMat(inv)
         inv
 }
